7e1ef7790976262a488714617dd4bdb3a113af0a
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-04-24 09:17:49 +0700

    test: Add ElementFactory.buildPoint test

--- src/factories/ElementFactory.test.ts
+++ src/factories/ElementFactory.test.ts
@@ -2,11 +2,31 @@ import { describe, expect, test } from "@jest/globals";
 import { ElementFactory } from "./ElementFactory";
 
 describe("ElementFactory", () => {
+  let factory: ElementFactory;
+
+  beforeAll(() => {
+    factory = ElementFactory.getInstance();
+  });
+
   describe("getInstance", () => {
     test("Should return the same instance", () => {
       expect(ElementFactory.getInstance()).toEqual(
         ElementFactory.getInstance()
       );
+    });
+  });
+
+  describe("buildPoint", () => {
+    test("Should return a Point with a name starting with P", () => {
+      const point1 = factory.buildPoint({ x: 10, y: 10 });
+
+      expect(point1.name.charAt(0)).toEqual("P");
+    });
+
+    test("Should return a Point with a name in increasing order", () => {
+      const point2 = factory.buildPoint({ x: 10, y: 10 });
+
+      expect(point2.name.charAt(1)).toEqual("2");
     });
   });
 });
