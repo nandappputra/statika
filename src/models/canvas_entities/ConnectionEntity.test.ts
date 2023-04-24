cd74398e962803a97e3f865f31de5e87e8ae435e
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-04-24 22:30:33 +0700

    test: Add test for ConnectionEntity.updatePosition

--- src/models/canvas_entities/ConnectionEntity.test.ts
+++ src/models/canvas_entities/ConnectionEntity.test.ts
@@ -6,6 +6,7 @@ import { ElementType } from "../../utils/Constants";
 import { setMockProperty } from "../../utils/TestUtils";
 import { Connection } from "../diagram_elements/connections/Connection";
 import { ConnectionEntity } from "./ConnectionEntity";
+import { MovePointEvent } from "../Event";
 
 describe("ExternalForceEntity", () => {
   let connection: MockedObject<Connection>;
@@ -37,6 +38,67 @@ describe("ExternalForceEntity", () => {
 
       expect(actualObjects.length).toBe(1);
       expect(actualObjects[0].data).toStrictEqual(expectedObject);
+    });
+  });
+
+  describe("updatePosition", () => {
+    test("Should update the position of the icon", () => {
+      const p1 = new Point("P1", 1, 1);
+      const p2 = new Point("P2", 2, 2);
+      setMockProperty(connection, "points", [p1, p2]);
+      setMockProperty(connection, "name", "C1");
+      eventMediator.updatePointPosition = jest.fn();
+
+      connectionEntity = new ConnectionEntity(connection, eventMediator);
+
+      const movePointEvent: MovePointEvent = {
+        name: "P1",
+        source: "user",
+        coordinate: { x: 20, y: 10 },
+      };
+
+      connectionEntity.updatePosition(movePointEvent);
+
+      const actualObjects = connectionEntity.getObjectsToDraw();
+
+      expect(actualObjects.length).toBe(1);
+      expect(actualObjects[0].left).toBe(20);
+      expect(actualObjects[0].top).toBe(10);
+    });
+
+    test("Should propagate the event to all points in the connection", () => {
+      const p1 = new Point("P1", 1, 1);
+      const p2 = new Point("P2", 2, 2);
+      setMockProperty(connection, "points", [p1, p2]);
+      setMockProperty(connection, "name", "C1");
+      const updatePosition =
+        jest.fn<(movePointEvent: MovePointEvent) => void>();
+      eventMediator.updatePointPosition = updatePosition;
+
+      connectionEntity = new ConnectionEntity(connection, eventMediator);
+
+      const movePointEvent: MovePointEvent = {
+        name: "P1",
+        source: "user",
+        coordinate: { x: 20, y: 10 },
+      };
+
+      connectionEntity.updatePosition(movePointEvent);
+
+      const expectedPropagatedEvent1: MovePointEvent = {
+        name: "P1",
+        source: "C1",
+        coordinate: { x: 20, y: 10 },
+      };
+      const expectedPropagatedEvent2: MovePointEvent = {
+        name: "P2",
+        source: "C1",
+        coordinate: { x: 20, y: 10 },
+      };
+
+      expect(updatePosition).toBeCalledTimes(2);
+      expect(updatePosition).toBeCalledWith(expectedPropagatedEvent1);
+      expect(updatePosition).toBeCalledWith(expectedPropagatedEvent2);
     });
   });
 });
