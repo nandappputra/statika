95f96e8bc869c1521292b255fab24525cc628716
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-04-25 17:45:29 +0700

    test: Add test for Point.removeExternalForce

--- src/models/Point.test.ts
+++ src/models/Point.test.ts
@@ -14,4 +14,20 @@ describe("Point", () => {
       expect(point.externalForces[0]).toBe(force);
     });
   });
+
+  describe("removeExternalForce", () => {
+    test("Should remove the correct force", () => {
+      const point = new Point("P1", 1, 2);
+      const force1 = new ExternalForce("F1", 30, 40);
+      const force2 = new ExternalForce("F2", 50, 60);
+
+      point.addExternalForce(force1);
+      point.addExternalForce(force2);
+
+      point.removeExternalForce(force1);
+
+      expect(point.externalForces.length).toBe(1);
+      expect(point.externalForces[0]).toBe(force2);
+    });
+  });
 });
