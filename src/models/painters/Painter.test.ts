19ff0537efd9907529836385fcc3081ff703129b
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-04-26 20:26:25 +0700

    test: Add test for Painter.removeElement

--- src/models/painters/Painter.test.ts
+++ src/models/painters/Painter.test.ts
@@ -2,13 +2,11 @@ import { jest, describe, expect, test } from "@jest/globals";
 import { MockedObject } from "jest-mock";
 import { LinkageElement } from "../diagram_elements/LinkageElement";
 import { Point } from "../Point";
-import { setMockProperty } from "../../utils/TestUtils";
 import { EntityConfig, Painter } from "./Painter";
 import { EventTrigger } from "./EventTrigger";
 import { EntityListFeature } from "./features/EntityListFeature";
 import { fabric } from "fabric";
 import { DiagramElement } from "../diagram_elements/DiagramElement";
-import { LinkageEntity } from "../canvas_entities/LinkageEntity";
 import { ConnectionElement } from "../diagram_elements/ConnectionElement";
 import { ConnectionType } from "../../utils/Constants";
 
@@ -66,6 +64,57 @@ describe("Painter", () => {
       painter.addElement(connection);
 
       expect(handleElementAddition).toBeCalledTimes(1);
+    });
+  });
+
+  describe("removeElement", () => {
+    test("Should remove the element and the points from the canvas", () => {
+      const point1 = new Point("P1", 1, 2);
+      const point2 = new Point("P2", 3, 4);
+      const linkage = new LinkageElement("L1", point1, point2);
+      const addToCanvas =
+        jest.fn<(...object: fabric.Object[]) => fabric.StaticCanvas>();
+      canvas.add = addToCanvas;
+      const handleElementAddition =
+        jest.fn<(painter: Painter, _element: DiagramElement) => void>();
+      feature.handleElementAddition = handleElementAddition;
+      const removeFromCanvas =
+        jest.fn<(...object: fabric.Object[]) => fabric.StaticCanvas>();
+      canvas.remove = removeFromCanvas;
+      const handleElementRemoval =
+        jest.fn<(painter: Painter, _element: DiagramElement) => void>();
+      feature.handleElementRemoval = handleElementRemoval;
+
+      painter.addElement(linkage);
+
+      painter.removeElement(linkage);
+
+      expect(removeFromCanvas).toBeCalledTimes(3);
+      expect(painter.getAllEntityName().length).toBe(0);
+    });
+
+    test("Should notify the features about the element addition", () => {
+      const point1 = new Point("P1", 1, 2);
+      const point2 = new Point("P2", 3, 4);
+      const linkage = new LinkageElement("L1", point1, point2);
+      const addToCanvas =
+        jest.fn<(...object: fabric.Object[]) => fabric.StaticCanvas>();
+      canvas.add = addToCanvas;
+      const handleElementAddition =
+        jest.fn<(painter: Painter, _element: DiagramElement) => void>();
+      feature.handleElementAddition = handleElementAddition;
+      const removeFromCanvas =
+        jest.fn<(...object: fabric.Object[]) => fabric.StaticCanvas>();
+      canvas.remove = removeFromCanvas;
+      const handleElementRemoval =
+        jest.fn<(painter: Painter, _element: DiagramElement) => void>();
+      feature.handleElementRemoval = handleElementRemoval;
+
+      painter.addElement(linkage);
+
+      painter.removeElement(linkage);
+
+      expect(handleElementRemoval).toBeCalledTimes(1);
     });
   });
 });
