40a08590974ef071957590bafe099c4a12b8efcf
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-05-05 20:15:42 +0700

    feat: Improved icon for fixed and horizontal roller icon

--- src/models/canvas_entities/ConnectionEntity.ts
+++ src/models/canvas_entities/ConnectionEntity.ts
@@ -6,6 +6,7 @@ import { EventMediator } from "../painters/EventMediator";
 import { Point } from "../Point";
 import { ConnectionType, ElementType } from "../../utils/Constants";
 import { ExternalForce } from "../ExternalForce";
+import { IGroupOptions } from "fabric/fabric-impl";
 
 export class ConnectionEntity implements CanvasEntity {
   private _name: string;
@@ -114,41 +115,9 @@ export class ConnectionEntity implements CanvasEntity {
   private buildFixedIcon(connection: ConnectionElement) {
     const reference = connection.points[0];
 
-    const baseLine = new fabric.Line(
-      [reference.x - 15, reference.y, reference.x + 15, reference.y],
-      {
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
+    const base = this.buildFixedBase(connection);
 
-    const line1 = new fabric.Line(
-      [reference.x - 8, reference.y, reference.x - 3, reference.y - 10],
-      {
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
-    const line2 = new fabric.Line(
-      [reference.x, reference.y, reference.x + 5, reference.y - 10],
-      {
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
-    const line3 = new fabric.Line(
-      [reference.x + 8, reference.y, reference.x + 13, reference.y - 10],
-      {
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
-
-    const icon = new fabric.Group([baseLine, line1, line2, line3], {
+    const icon = new fabric.Group([base], {
       originX: "center",
       originY: "bottom",
       left: reference.x,
@@ -204,82 +173,56 @@ export class ConnectionEntity implements CanvasEntity {
     const reference = connection.points[0];
 
     const box = new fabric.Rect({
-      originY: "bottom",
+      originY: "center",
       originX: "center",
       left: reference.x,
       top: reference.y,
       width: 30,
-      height: 6,
-      fill: "white",
+      height: 40,
+      visible: false,
     });
-    const baseLine = new fabric.Line(
-      [
-        reference.x - 15,
-        reference.y - 2.5,
-        reference.x + 15,
-        reference.y - 2.5,
-      ],
-      {
-        originX: "center",
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
 
-    const line1 = new fabric.Line(
-      [reference.x - 8, reference.y - 4, reference.x - 3, reference.y - 10],
-      {
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
-    const line2 = new fabric.Line(
-      [reference.x, reference.y - 4, reference.x + 5, reference.y - 10],
-      {
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
-    const line3 = new fabric.Line(
-      [reference.x + 8, reference.y - 4, reference.x + 13, reference.y - 10],
-      {
-        stroke: "black",
-        strokeWidth: 3,
-        strokeLineCap: "round",
-      }
-    );
+    const base = this.buildFixedBase(connection, {
+      top: reference.y - 25,
+    });
 
-    const roller = new fabric.Circle({
+    const ellipse = new fabric.Ellipse({
+      fill: "white",
+      stroke: "black",
+      strokeWidth: 3,
       originX: "center",
+      originY: "center",
+      width: 20,
+      height: 20,
+      left: reference.x,
+      top: reference.y - 4,
+      rx: 6.5,
+      ry: 10,
+    });
+
+    const circle = new fabric.Circle({
       fill: "white",
       stroke: "black",
       strokeWidth: 3,
       radius: 5,
+      originX: "center",
+      originY: "center",
+      left: reference.x,
+      top: reference.y,
+    });
+
+    return new fabric.Group([box, ellipse, base, circle], {
+      originX: "center",
+      originY: "center",
       left: reference.x,
       top: reference.y,
       hasControls: false,
+      data: {
+        name: connection.name,
+        pointName: connection.points[0].name,
+        type: ElementType.CONNECTION,
+      },
     });
-
-    const icon = new fabric.Group(
-      [box, baseLine, line1, line2, line3, roller],
-      {
-        originX: "center",
-        originY: "center",
-        left: reference.x,
-        top: reference.y - 4,
-        hasControls: false,
-        data: {
-          name: connection.name,
-          pointName: connection.points[0].name,
-          type: ElementType.CONNECTION,
-        },
-      }
-    );
-
-    return icon;
   }
 
   private buildPinJointIcon(connection: ConnectionElement) {
@@ -302,21 +245,109 @@ export class ConnectionEntity implements CanvasEntity {
   }
 
   private buildPinIcon(connection: ConnectionElement) {
-    return new fabric.Circle({
+    const reference = connection.points[0];
+    const box = new fabric.Rect({
+      originY: "center",
+      originX: "center",
+      left: reference.x,
+      top: reference.y,
+      width: 30,
+      height: 40,
+      visible: false,
+    });
+
+    const base = this.buildFixedBase(connection, {
+      top: reference.y - 25,
+    });
+
+    const triangle = new fabric.Triangle({
+      fill: "white",
+      stroke: "black",
+      strokeWidth: 3,
+      originX: "center",
+      originY: "center",
+      angle: 180,
+      width: 20,
+      height: 20,
+      left: reference.x,
+      top: reference.y - 5,
+    });
+
+    const circle = new fabric.Circle({
       fill: "white",
       stroke: "black",
       strokeWidth: 3,
       radius: 5,
       originX: "center",
       originY: "center",
-      left: connection.points[0].x,
-      top: connection.points[0].y,
+      left: reference.x,
+      top: reference.y,
+    });
+
+    return new fabric.Group([box, triangle, base, circle], {
+      originX: "center",
+      originY: "center",
+      left: reference.x,
+      top: reference.y,
+      hasControls: false,
       data: {
         name: connection.name,
         pointName: connection.points[0].name,
         type: ElementType.CONNECTION,
       },
-      hasControls: false,
     });
+  }
+
+  private buildFixedBase(
+    connection: ConnectionElement,
+    options: IGroupOptions | undefined = undefined
+  ) {
+    const reference = connection.points[0];
+
+    const box = new fabric.Rect({
+      originY: "bottom",
+      originX: "center",
+      left: reference.x,
+      top: reference.y,
+      width: 30,
+      height: 6,
+      fill: "white",
+    });
+    const baseLine = new fabric.Line(
+      [reference.x - 15, reference.y, reference.x + 15, reference.y],
+      {
+        originX: "center",
+        stroke: "black",
+        strokeWidth: 3,
+        strokeLineCap: "round",
+      }
+    );
+
+    const line1 = new fabric.Line(
+      [reference.x - 13, reference.y, reference.x - 8, reference.y - 10],
+      {
+        stroke: "black",
+        strokeWidth: 3,
+        strokeLineCap: "round",
+      }
+    );
+    const line2 = new fabric.Line(
+      [reference.x - 2, reference.y, reference.x + 3, reference.y - 10],
+      {
+        stroke: "black",
+        strokeWidth: 3,
+        strokeLineCap: "round",
+      }
+    );
+    const line3 = new fabric.Line(
+      [reference.x + 8, reference.y, reference.x + 13, reference.y - 10],
+      {
+        stroke: "black",
+        strokeWidth: 3,
+        strokeLineCap: "round",
+      }
+    );
+
+    return new fabric.Group([box, baseLine, line1, line2, line3], options);
   }
 }
